ea8b2dee82818e481bedcdb77d9b7fa34b841f37
unknown <jlevien808@gmail.com>
2020-10-24 11:53:23 -1000

    Contact update

--- src/Layout/AppHeader/Components/HeaderDots.js
+++ src/Layout/AppHeader/Components/HeaderDots.js
@@ -75,7 +75,7 @@ function getTabs() {
     title: tab.title,
     getContent: () => tab.content,
     key: index,
-  }));
+  }));  
 }
 
 class HeaderDots extends React.Component {
@@ -90,65 +90,9 @@ class HeaderDots extends React.Component {
     return (
       <Fragment>
         <div className="header-dots">
-          <UncontrolledDropdown>
-            <DropdownToggle className="p-0" color="link">
-              <div className="icon-wrapper icon-wrapper-alt rounded-circle">
-                <div className="icon-wrapper-bg bg-success" />
-                <IoIosAnalytics color="#3ac47d" fontSize="23px" />
-              </div>
-            </DropdownToggle>
-            <DropdownMenu right className="dropdown-menu-xl rm-pointers">
-              <div className="dropdown-menu-header">
-                <div className="dropdown-menu-header-inner bg-premium-dark">
-                  <div className="menu-header-image"
-                    style={{
-                      backgroundImage: "url(" + bg4 + ")",
-                    }}/>
-                  <div className="menu-header-content text-white">
-                    <h5 className="menu-header-title">Reach Count</h5>
-                    <h6 className="menu-header-subtitle">
-                      PCP Users.
-                    </h6>
-                  </div>
-                </div>
-              </div>
-              <div className="widget-chart">
-                <div className="widget-chart-content">
-                  <div className="icon-wrapper rounded-circle">
-                    <div className="icon-wrapper-bg opacity-9 bg-focus" />
-                    <i className="lnr-users text-white" />
-                  </div>
-                  <div className="widget-numbers">
-                    <CountUp start={0} end={6.174} separator="" decimals={3} decimal="."
-                      prefix="" useEasing={false} suffix= "m" duration="3"/>
-                  </div>
-                  <div className="widget-subheading pt-2">
-                    Accumilated reach of PCP.
-                  </div>
-                  <div className="widget-description text-white">
-                    <span className="pr-1">
-                      <CountUp start={0} end={176} separator="," delay={2} decimals={0} decimal=","
-                        useEasing={false} prefix="" suffix="%" duration="10"/>
-                    </span>
-                    <FontAwesomeIcon icon={faArrowLeft} />
-                  </div>
-                </div>
-                <div className="widget-chart-wrapper">
-                  <ResponsiveContainer width="100%" aspect={3.0 / 1.0}>
-                    <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-                      <Area type="monotoneX" dataKey="uv" stroke="var(--warning)" fill="var(--warning)" fillOpacity=".5"/>
-                    </AreaChart>
-                  </ResponsiveContainer>
-                </div>
-              </div>
-              <Nav vertical>
-                <NavItem className="nav-item-divider mt-0"> </NavItem>
-                <NavItem className="nav-item-btn text-center">
-                  
-                </NavItem>
-              </Nav>
-            </DropdownMenu>
-          </UncontrolledDropdown>
+          
+<Button disabled> Login Coming Soon </Button>
+
         </div>
       </Fragment>
     );
