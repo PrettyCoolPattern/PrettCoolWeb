9dc5d4fa78ec2761731432f35968945765c038da
unknown <jlevien808@gmail.com>
2021-01-01 12:33:00 -1000

    Add LandingPage Loader

--- src/Layout/AppMain/index.js
+++ src/Layout/AppMain/index.js
@@ -16,158 +16,183 @@ const Components = lazy(() => import("../../Pages/Components"));
 const Forms = lazy(() => import("../../Pages/Forms"));
 const Tables = lazy(() => import("../../Pages/Tables"));
 
-
-
 const AppMain = () => {
+  return (
+    <Fragment>
+      {/* Components */}
 
-    return (
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="ball-pulse-rise" />
+              </div>
+              <h6 className="mt-5">
+                Loading Components
+                <small>Welcome to PrettyCoolPattern</small>
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/components" component={Components} />
+      </Suspense>
 
-        <Fragment>
+      {/* Forms */}
 
-            {/* Components */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="ball-pulse-rise" />
+              </div>
+              <h6 className="mt-5">
+                Loading Forms
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/forms" component={Forms} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-pulse-rise"/>
-                        </div>
-                        <h6 className="mt-5">
-                               Loading Components
-                            <small>Welcome to PrettyCoolPattern</small></h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/components" component={Components}/>
-            </Suspense>
+      {/* Tables */}
 
-            {/* Forms */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="ball-pulse-rise" />
+              </div>
+              <h6 className="mt-5">
+                Loading Tables
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/tables" component={Tables} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-pulse-rise"/>
-                        </div>
-                        <h6 className="mt-5">
-                                  Loading Forms
-                            <small>Welcome to PrettyCoolPattern</small>  </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/forms" component={Forms}/>
-            </Suspense>
+      {/* Elements */}
 
-       
-            {/* Tables */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="line-scale" />
+              </div>
+              <h6 className="mt-3">
+                Loading Elements
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/elements" component={Elements} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-pulse-rise"/>
-                        </div>
-                        <h6 className="mt-5">
-                        Loading Tables
-                            <small>Welcome to PrettyCoolPattern</small>    </h6>
-                 
-                    </div>
-                </div>
-            }>
-                <Route path="/tables" component={Tables}/>
-            </Suspense>
+      {/* Dashboard Widgets */}
 
-            {/* Elements */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="ball-pulse-sync" />
+              </div>
+              <h6 className="mt-3">
+                Loading Widgets
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/widgets" component={Widgets} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="line-scale"/>
-                        </div>
-                        <h6 className="mt-3">
-                        Loading Elements
-                            <small>Welcome to PrettyCoolPattern</small>    </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/elements" component={Elements}/>
-            </Suspense>
+      {/* Pages */}
 
-            {/* Dashboard Widgets */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="line-scale-party" />
+              </div>
+              <h6 className="mt-3">
+                Loading Pages
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/pages" component={UserPages} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-pulse-sync"/>
-                        </div>
-                        <h6 className="mt-3">
-                        Loading Widgets
-                            <small>Welcome to PrettyCoolPattern</small> </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/widgets" component={Widgets}/>
-            </Suspense>
+      {/* Applications */}
 
-            {/* Pages */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader type="ball-pulse" />
+              </div>
+              <h6 className="mt-3">
+                Loading Application Tools
+                <small>Welcome to PrettyCoolPattern</small>{" "}
+              </h6>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/apps" component={Applications} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="line-scale-party"/>
-                        </div>
-                        <h6 className="mt-3">
-                        Loading Pages
-                            <small>Welcome to PrettyCoolPattern</small>   </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/pages" component={UserPages}/>
-            </Suspense>
+      {/* Dashboards */}
 
-            {/* Applications */}
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="loader-container-inner">
+              <div className="text-center">
+                <Loader
+                  style={{
+                    transform: "scale(5.5)",
+                    top: "-100px",
+                    position: "relative",
+                    color: "rgb(156, 22, 156)",
+                  }}
+                  type="ball-clip-rotate-multiple"
+                />
+              </div>
+              <h2 className="mt-3" style={{ color: "white" }}>
+                Loading Dashboard Tools
+                <small style={{ color: "white" }}>
+                  Welcome to PrettyCoolPattern
+                </small>
+              </h2>
+            </div>
+          </div>
+        }
+      >
+        <Route path="/dashboards" component={Dashboards} />
+      </Suspense>
 
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-pulse"/>
-                        </div>
-                        <h6 className="mt-3">
-                        Loading Application Tools
-                            <small>Welcome to PrettyCoolPattern</small> </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/apps" component={Applications}/>
-            </Suspense>
-
-            {/* Dashboards */}
-
-            <Suspense fallback={
-                <div className="loader-container">
-                    <div className="loader-container-inner">
-                        <div className="text-center">
-                            <Loader type="ball-grid-cy"/>
-                        </div>
-                        <h6 className="mt-3">
-                            Loading Dashboard Tools
-                            <small>Welcome to PrettyCoolPattern</small>
-                        </h6>
-                    </div>
-                </div>
-            }>
-                <Route path="/dashboards" component={Dashboards}/>
-            </Suspense>
-
-
-            <Route exact path="/" component={LandingPage} />
-            <ToastContainer/>
-        </Fragment>
-    )
+      <Route exact path="/" component={LandingPage} />
+      <ToastContainer />
+    </Fragment>
+  );
 };
 
 export default AppMain;
